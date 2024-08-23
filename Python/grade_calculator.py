a094fc10b00b5ec0562dffa360637b56c5440b27
moriegoba19 <moriegoba19@hotmail.com>
2024-08-23 18:48:25 -0400

    first commit

---
+++ Python/grade_calculator.py
@@ -0,0 +1,7 @@
+"""
+A Python program to calculate and format grades.
+Author: Martin Goba
+Course: Software Engineering Bootcamp
+Date: 08/23/2024
+Description: This program calculates the average and final letter grades based on user-inputted grades.
+"""
